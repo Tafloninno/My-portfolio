de5fee245fb9be3f9f5ddddc30749c35e8aba5b8
Mugisha Innocent <tafloninno@gmail.com>
2024-04-09 15:56:44 -0400

    Update Portfolio.jsx

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -27,7 +27,7 @@ const Portfolio = () => {
       stack: 'JavaScript, HTML, CSS'
     },
     { id: 4, 
-      src: SkyCast, 
+      src: Skycast, 
       text: ' A weather App  using  API to display cities weather and give detail data for every city.',
       title: 'weather App',
       link1: 'https://66159969d9897b293763540f--silver-buttercream-ab2563.netlify.app/',
